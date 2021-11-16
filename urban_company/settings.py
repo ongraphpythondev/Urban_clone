aff96c96adc0c1e88aa918569887e51c0e32232e
unknown <riteshpandey1200@gmail.com>
2021-11-16 10:30:34 +0530

    update

--- urban_company/settings.py
+++ urban_company/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'riteshpandey1200@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pandey123456*'
+EMAIL_HOST_PASSWORD = 'Pandey252110'
