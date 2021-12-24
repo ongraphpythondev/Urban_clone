b4c373b2ecdc68fcdfe903bfed5b5d1753a029c6
unknown <riteshpandey1200@gmail.com>
2021-12-24 11:28:09 +0530

    now deploying to heroku

--- urban_company/settings.py
+++ urban_company/settings.py
@@ -145,7 +145,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email' # your email
-EMAIL_HOST_PASSWORD = 'password' # your password
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"] # your email
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"] # your password
 
 
